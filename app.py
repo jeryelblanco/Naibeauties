2117525fb580822f758cf25c9cf4f0e7a79255e2
jeryelblanco <94143119+jeryelblanco@users.noreply.github.com>
2024-04-17 10:38:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -114,4 +114,4 @@ def serve_image():
 
 ## this is where we run the server, no port is specified 
 if __name__ == "__main__":
-        app.run(host = '0.0.0.0', port =32583)
+        app.run(host = '0.0.0.0')
